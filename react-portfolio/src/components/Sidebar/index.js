71a0b6567e329e169c4bae59b3b53016c56681f9
Your Name~ <you@example.com>
2022-07-07 23:59:04 +0100

    Typos fixed

--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -6,26 +6,28 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png' 
 
 
-const Sidebar = () => (
-  <div classname= "nav-bar">
-    <Link classname='logo' to= '/'>
-      <img src={LogoS} alt="logo"/> 
-      <img classname="sub-logo" src={LogoSubtitle} alt="Teacher to Student"/> 
-    </Link>
-    <nav>
-      <NavLink extract="true" activeclassname="active" to='/'>
-        {/* <FontAwesomeIcon icon={faHome} color="#4d4d4e" /> */}
-      </NavLink>
+const Sidebar = () => {
+  return (
+    <div classname="nav-bar">
+      <Link classname='logo' to='/'>
+        <img src={LogoS} alt="logo" />
+        <img classname="sub-logo" src={LogoSubtitle} alt="Teacher to Student" />
+      </Link>
+      <nav>
+        <NavLink extract="true" activeclassname="active" to='/'>
+          {/* <FontAwesomeIcon icon={faHome} color="#4d4d4e" /> */}
+        </NavLink>
 
-      <NavLink extract="true" activeclassname="active" classname="about-link" to='/about'>
-        {/* <FontAwesomeIcon icon={faUser} color="#4d4d4e" /> */}
-      </NavLink>
+        <NavLink extract="true" activeclassname="active" classname="about-link" to='/about'>
+          {/* <FontAwesomeIcon icon={faUser} color="#4d4d4e" /> */}
+        </NavLink>
 
-      <NavLink extract="true" activeclassname="active" classname="contact-link" to='/contact'>
-        {/* <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" /> */}
-      </NavLink>
-    </nav>
+        <NavLink extract="true" activeclassname="active" classname="contact-link" to='/contact'>
+          {/* <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" /> */}
+        </NavLink>
+      </nav>
     </div>
-    )
+  )
+}
 
 export default Sidebar
